4d9c1c9a3e6b64183f12b70e6b65c6ac572d2cfc
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-02-13 20:50:07 -0600

    Exercise 1.1 completed

--- intro.js
+++ intro.js
@@ -1,6 +1,12 @@
 console.log("Hello World!");
 // console.log is often used to test code snippets and see results.
 
-console.table(); // Creates a table when the inputted data  an be presented as a table.
+// console.table(); Creates a table when the inputted data  an be presented as a table.
 
-console.error(); // Will log the inputted data, but with a styling that draws attention to the fact that it's an error.
+// console.error(); Will log the inputted data, but with a styling that draws attention to the fact that it's an error.
+
+// Practice exercise 1.1
+
+console.log(10+4);
+
+console.log("Bryan Garduno Gonzalez");
