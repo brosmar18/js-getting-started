c57e0e921b200dae1fc7f2205670b10c0bce7e4a
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-02-13 20:46:42 -0600

    console.log()

---
+++ intro.js
@@ -0,0 +1,6 @@
+console.log("Hello World!");
+// console.log is often used to test code snippets and see results.
+
+console.table(); // Creates a table when the inputted data  an be presented as a table.
+
+console.error(); // Will log the inputted data, but with a styling that draws attention to the fact that it's an error.
